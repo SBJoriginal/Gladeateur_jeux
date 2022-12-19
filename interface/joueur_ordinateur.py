8fc1d1fdd9ea49bfc5e6482b04e008f5250872b9
Nimaga95 <117949986+Nimaga95@users.noreply.github.com>
2022-12-19 07:09:38 -0500

    Config, et Joeur odinateur
    Config en cours : 70% fait
    
    - Les 4 méhodes: joeur odinateur fini presque, il reste verification

--- interface/joueur_ordinateur.py
+++ interface/joueur_ordinateur.py
@@ -10,11 +10,32 @@ from jeu.joueur import Joueur
 
 #### DÉBUT DÉFI JOUEUR ORDINATEUR ####
 
-class JoueurOrdinateur:  # N'oubliez pas d'hériter de Joueur !!
+class JoueurOrdinateur(Joueur):  # N'oubliez pas d'hériter de Joueur !!
+    """
+    # Args : numero joeur, des_initiaux, arene
+
+    """
+
     def __init__(self, numero_joueur, des_initiaux, arene):
         # Supprimer la ligne du raise, et ajoutez l'appel à super().__init__
-        raise ValueError("Le défi Joueur Ordinateur n'a pas encore été réalisé!")
+        super().__init__(numero_joueur, des_initiaux, arene)
+        # raise ValueError("Le défi Joueur Ordinateur n'a pas encore été réalisé!")
 
     # Écrivez les 4 méthodes demandées.
+
+    def choisir_coordonnees(self):
+        return super().piger_coordonnees()
+
+    def choisir_angle(self):
+        return super().piger_angle()
+
+    def choisir_puissance(self):
+        return super().piger_puissance()
+
+    def decision_continuer(self):
+        if randint(0, 3) != 0:
+            return True
+        else:
+            return False
 
 #### FIN DÉFI JOUEUR ORDINATEUR ####
